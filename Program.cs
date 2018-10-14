4fe69b138af0a32b4dad10f85b09b9f0111a83ed
Simon Wahlin <simon.wahlin@knowledgefactory.se>
2018-10-14 21:42:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS2exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
