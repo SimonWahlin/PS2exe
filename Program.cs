c9bf3ec8fb03f52230fea5fe1c37640352e4d507
Simon Wahlin <simon.wahlin@knowledgefactory.se>
2018-10-15 08:01:39 +0200

    New project with references

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 
 namespace PS2exe
 {
